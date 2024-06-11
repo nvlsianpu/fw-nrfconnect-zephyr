8d59cfec195de4392726ad9e26dda97fb8df823c
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2024-06-11 16:40:37 +0200

    NCSDK-27931: try attack
    tbd
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -9,6 +9,20 @@
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
-
+	atack_key();
 	return 0;
+}
+
+#include "bl_storage.h"
+#include <nrfx_nvmc.h>
+
+void atack_key(void)
+{
+	const volatile uint32_t *invalidation_token =
+			&BL_STORAGE->key_data[0].valid;
+
+	/* Write if not already written. */
+	nrfx_nvmc_word_write((uint32_t)invalidation_token, 0x0000FFFF);
+	printf("invalidation token atack: 0x%#08x\n", (uint32_t)invalidation_token);
+	while(1);
 }
